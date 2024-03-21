007d0412e906374980428174b1ed1106f7e94271
Ha Van Thuy/대리/메가존_MZ HMI <thuyhv@mz.co.kr>
2024-03-21 15:39:03 +0700

    Configure to run Diagnose stability - run below command for release build variant:  ./gradlew assembleRelease -PcomposeCompilerReports=true - You can find the output at <ProjectDir>/app/build/compose-compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            if (project.findProperty("composeCompilerReports") == "true") {
+                freeCompilerArgs += listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
+                )
+            }
+            if (project.findProperty("composeCompilerMetrics") == "true") {
+                freeCompilerArgs += listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
+                )
+            }
+        }
+    }
+}
